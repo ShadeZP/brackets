ee80f8f5ddc70c4e616223ec3cd486164ae98dd3
Evgeniy Vasilko <shadezp@gmail.com>
2020-03-12 22:56:12 +0200

    feat: add partial solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    str = str.split('');
+    for (let i=0; i<str.length; i++){
+        for (let j=0; j<bracketsConfig.length; j++) {
+            if (str[i]==bracketsConfig[j][0]&&str[i+1]==bracketsConfig[j][1]) {
+                str.splice (i, 2);
+                i=0;
+                j=0;
+            }
+        }
+    }
+    if (str.length==0) {
+        return true;
+    };
+    return false;
 }
